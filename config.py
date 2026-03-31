04aebfff5a716b93f7cfd59d387c20c008ae2ad6
Dukhvaha <pitparker500@gmail.com>
2026-03-31 03:22:32 +0300

    fix:hide secrets

--- config.py
+++
@@ -1,6 +0,0 @@
-BOT_API_TOKEN = "8768025438:AAGcQSYPPgYJ0aajRhUcimxV8EwXYmURmZM"
-CHANNEL_BOT_ID = "-1002995237562"
-KINOPOISK_API_KEY = '9ce06597-b18a-40df-9fb6-3c21e93bc625'
-KINOPOISK_URL = 'https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword'
-BASE_URL = 'https://catalytical-cuticolor-della.ngrok-free.dev'
-BACKEND_URL = 'http://localhost:8000'
